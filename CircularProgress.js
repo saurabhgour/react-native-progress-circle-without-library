8c3a519c7fd3f2d5e47e294c5f14eb3eb69e93b9
saurabhgour <33369861+saurabhgour@users.noreply.github.com>
2018-06-30 20:26:53 +0530

    Create CircularProgress.js

---
+++ CircularProgress.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+/**
+* Override styles that get passed from props
+**/
+propStyle = (percent, base_degrees) => {
+  const rotateBy = base_degrees + (percent * 3.6);
+  return {
+    transform:[{rotateZ: `${rotateBy}deg`}]
+  };
+}
+
+renderThirdLayer = (percent) => {
+  if(percent > 50){
+    /**
+    * Third layer circle default is 45 degrees, so by default it occupies the right half semicircle.
+    * Since first 50 percent is already taken care  by second layer circle, hence we subtract it
+    * before passing to the propStyle function
+    **/
+    return <View style={[styles.secondProgressLayer,propStyle((percent - 50), 45) ]}></View>
+  }else{
+    return <View style={styles.offsetLayer}></View>
+  }
+}
+
+const CircularProgress = ({percent}) => {
+  let firstProgressLayerStyle;
+  if(percent > 50){
+      firstProgressLayerStyle = propStyle(50, -135);
+  }else {
+    firstProgressLayerStyle = propStyle(percent, -135);
+  }
+
+  return(
+    <View style={styles.container}>
+      <View style={[styles.firstProgressLayer, firstProgressLayerStyle]}></View>
+      {renderThirdLayer(percent)}
+      <Text style={styles.display}>{percent}%</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 200,
+    height: 200,
+    borderWidth: 20,
+    borderRadius: 100,
+    borderColor: 'grey',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  firstProgressLayer: {
+    width: 200,
+    height: 200,
+    borderWidth: 20,
+    borderRadius: 100,
+    position: 'absolute',
+    borderLeftColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderRightColor: '#3498db',
+    borderTopColor: '#3498db',
+    transform:[{rotateZ: '-135deg'}]
+  },
+  secondProgressLayer:{
+    width: 200,
+    height: 200,
+    position: 'absolute',
+    borderWidth: 20,
+    borderRadius: 100,
+    borderLeftColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderRightColor: '#3498db',
+    borderTopColor: '#3498db',
+    transform: [{rotateZ: '45deg'}]
+  },
+  offsetLayer: {
+    width: 200,
+    height: 200,
+    position: 'absolute',
+    borderWidth: 20,
+    borderRadius: 100,
+    borderLeftColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderRightColor: 'grey',
+    borderTopColor: 'grey',
+    transform:[{rotateZ: '-135deg'}]
+  },
+  display: {
+    position: 'absolute',
+    fontSize: 40,
+    fontWeight: 'bold'
+  }
+});
+
+export default CircularProgress;
